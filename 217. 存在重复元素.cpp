84767dfe1d1ddcbf674b9eef6dc30fd1c83df6fc
zhoutao000815 <1761885173@qq.com>
2021-02-14 14:09:43 +0800

    leetcode：217. 存在重复元素

---
+++ 217. 存在重复元素.cpp
@@ -0,0 +1,40 @@
+﻿//217. 存在重复元素
+//给定一个整数数组，判断是否存在重复元素。
+//
+//如果存在一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+//
+//示例 1:
+//
+//输入 : [1, 2, 3, 1]
+//输出 : true
+//	 示例 2 :
+//
+// 输入 : [1, 2, 3, 4]
+//  输出 : false
+//	   示例 3 :
+//
+//   输入 : [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+//	输出 : true
+//
+//
+//		  来源：力扣（LeetCode）
+//	  链接：https ://leetcode-cn.com/problems/contains-duplicate
+//	 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+#include<stdio.h>
+#include<Windows.h>
+
+int cmp(const void* _a, const void* _b) {
+	int a = *(int*)_a, b = *(int*)_b;
+	return a - b;
+}
+//排序后，双指针查重即可。
+bool containsDuplicate(int* nums, int numsSize) {
+	qsort(nums, numsSize, sizeof(int), cmp);
+	for (int i = 0; i < numsSize - 1; i++) {
+		if (nums[i] == nums[i + 1]) {
+			return true;
+		}
+	}
+	return false;
+}
+//方法二：哈希表，
